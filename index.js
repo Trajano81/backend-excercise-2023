740917ae15c34007df8a029fa86e259484328e71
Trajano81 <113276131+Trajano81@users.noreply.github.com>
2023-10-18 00:23:19 +0000

    Create endpoint `/oi`

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+app.get('/oi', function (req, res) {
+  res.send('Olá Mundo')
+})
 
-app.listen(3000)
+
+app.listen(3000, function(){
+  console.log('App running on http://localhost:3000')
+})
