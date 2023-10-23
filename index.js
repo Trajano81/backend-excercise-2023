00c491caa92036e968c52d9724ef8eea2f363645
Trajano81 <113276131+Trajano81@users.noreply.github.com>
2023-10-23 01:32:04 +0000

    Implement endpoint `READ ALL`

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.get('/oi', function (req, res) {
   res.send('Olá Mundo')
 })
 
+// CRUD list of creatures
+const items = ['java','android','kotlin']
+
+// READ ALL - GET (Items)
+app.get('/items', function(req, res){
+  res.send(items)
+})
 
 app.listen(3000, function(){
   console.log('App running on http://localhost:3000')
