0d63b96398b0bb43d23ef7cdb8a0cd7e2d0787ad
Trajano81 <113276131+Trajano81@users.noreply.github.com>
2023-10-23 01:58:35 +0000

    Implement endpoint `Create` `[POST] /items `

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+// The body from thr request is always json
+app.use(express.json())
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
@@ -11,9 +14,19 @@ app.get('/oi', function (req, res) {
 // CRUD list of creatures
 const items = ['java','android','kotlin']
 
-// READ ALL - GET (Items)
+// READ ALL - GET / (Items)
 app.get('/items', function(req, res){
   res.send(items)
+})
+
+// CREATE - POST / items
+app.post('/items', function(req, res){
+  const item=req.body.nome
+
+  //Insert into list
+  items.push(item)
+
+  res.send("Item inserted succesfully")
 })
 
 app.listen(3000, function(){
